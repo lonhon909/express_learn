a66b5987a176a0a03074b4a9a46bc6a38344928c
lonhon909 <ttabj@sina.com>
2019-04-26 15:13:14 +0800

    post

--- src/utils/post.js
+++ src/utils/post.js
@@ -1,13 +1,41 @@
+function getData() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        error: 0,
+        data: 'req.body',
+        msg: '登陆成功'
+      })
+    }, 500)
+  })
+}
+
+function getlist() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        error: 0,
+        data: 'req.body',
+        msg: '登陆成功'
+      })
+    }, 5000)
+  })
+}
+
 module.exports = [
   {
     url: '/login',
-    callback: function(req, res) {
+    callback: async function(req, res) {
       console.log(req.body)
-      res.send({
-        error: 0,
-        data: req.body,
-        msg: '登陆成功'
-      })
+      let response = await getData()
+      res.send(response)
+    }
+  },
+  {
+    url: '/user.do',
+    callback: async function(req, res) {
+      let response = await getlist();
+      res.send(response);
     }
   }
 ]
